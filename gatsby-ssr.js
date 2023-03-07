4dbbabcf9dc7b1173952d2079f5b7adbf86237ff
River0f <r1ver0ffundm4nt@gmail.com>
2023-03-07 08:32:02 +0100

    first commit

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -9,4 +9,20 @@
  */
 exports.onRenderBody = ({ setHtmlAttributes }) => {
   setHtmlAttributes({ lang: `en` })
+  setHeadComponents([
+    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"/>,
+    <script dangerouslySetInnerHTML={{
+      __html: `if (window.netlifyIdentity) {
+        window.netlifyIdentity.on("init", user => {
+          if (!user) {
+            window.netlifyIdentity.on("login", () => {
+              document.location.href = "/admin/";
+            });
+          }
+        })`
+      }
+    } />
+
+
+  ])
 }
